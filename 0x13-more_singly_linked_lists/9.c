84ddc16b1be92b26f48e7c34f62102af30bf95ec
Baddi Mohamed <mehamad199523@gmail.com>
2023-08-01 02:53:42 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/9.c
+++ 0x13-more_singly_linked_lists/9.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "../lists.h"
+#include "lists.h"
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
